a8376b1c099908f13d3ca5514aac2582248e8608
SinckCode <soyangeldavid1@gmail.com>
2025-07-16 00:57:40 -0600

    Subiendo api cambios

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,12 @@ module.exports = {
   apps: [
     {
       name: 'mi-api',
-      script: './src/app.js', // ← ruta correcta del archivo
+      script: './src/app.js',
       watch: false,
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
-        DEPLOY_SECRET: 'a98f3hT97hfd-23HFh90+ffd392_HFD' // ← aquí va tu secreto
+        DEPLOY_SECRET: 'a98f3hT97hfd-23HFh90+ffd392_HFD'
       }
     }
   ]
